2d6fda692762131df13929c2f934cb9da79fde3c
aguayo16 <98487148+aguayo16@users.noreply.github.com>
2022-03-19 02:10:18 -0400

    split function/set eventlistener for target values

---
+++ client/lab_7/script.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+
+function dataHandler(array) {
+  console.table(array);
+  const range = [...Array(15).keys()];
+  // sets a random list of restaurant names
+  const listItems = range.map((item, index) => {
+    const randNum = randomize(0, array.length - 1);
+    return array[randNum];
+  });
+  return listItems;
+}
+
+// injects the random array into the list
+function injectRandRestList(array) {
+  document.querySelector('.resto-list').innerHTML = '';
+  array.forEach((item) => {
+    const restName = item.name.toLowerCase();
+    const injectRestName = `<li>${restName}</li>`;
+    document.querySelector('.resto-list').innerHTML += injectRestName;
+  });
+}
+
+// function that randomizes given a min and a max
+function randomize(min, max) {
+  minimum = Math.ceil(min);
+  maximum = Math.floor(max);
+  return Math.floor(
+    Math.random() * (maximum - minimum + 1) + min
+  );
+}
+
+async function mainEvent() { // the async keyword means we can make API requests
+  const form = document.querySelector('.user-form');
+  const submitButton = document.querySelector('button');
+
+  const restNameInput = document.querySelector('#rest-name');
+  const restCityInput = document.querySelector('#city');
+
+  // submitButton.style.display = 'none';
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
+  if (arrayFromJson.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      submitButton.style.display = 'block';
+
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      injectRandRestList(dataHandler(arrayFromJson));
+    });
+
+    let currentArray;
+    restNameInput.addEventListener('input', (e) => {
+      console.log(e.target.value);
+    });
+  }
+}
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
