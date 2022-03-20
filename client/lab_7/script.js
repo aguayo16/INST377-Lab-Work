5f4536f04e4f43217025d1f0a4b38e9fa19f8392
aguayo16 <98487148+aguayo16@users.noreply.github.com>
2022-03-20 04:48:06 -0400

    finished filtering, fixed user-form

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable array-callback-return */
+/* eslint-disable arrow-body-style */
+/* eslint-disable prefer-const */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
-
+let randArray = [];
 function dataHandler(array) {
-  console.table(array);
+  // console.table(array);
   const range = [...Array(15).keys()];
   // sets a random list of restaurant names
   const listItems = range.map((item, index) => {
@@ -17,8 +21,11 @@ function injectRandRestList(array) {
   document.querySelector('.resto-list').innerHTML = '';
   array.forEach((item) => {
     const restName = item.name.toLowerCase();
+    const restZipCode = item.zip;
     const injectRestName = `<li>${restName}</li>`;
+    const injectZipCode = `<li class = 'li-zip'>${restZipCode}</li>`;
     document.querySelector('.resto-list').innerHTML += injectRestName;
+    document.querySelector('.resto-list').innerHTML += injectZipCode;
   });
 }
 
@@ -36,26 +43,62 @@ async function mainEvent() { // the async keyword means we can make API requests
   const submitButton = document.querySelector('button');
 
   const restNameInput = document.querySelector('#rest-name');
-  const restCityInput = document.querySelector('#city');
+  const zipCodeInput = document.querySelector('#zip-code');
 
-  // submitButton.style.display = 'none';
+  submitButton.style.display = 'none';
   const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
 
   if (arrayFromJson.length > 0) {
+    submitButton.style.display = 'block';
+
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      submitButton.style.display = 'block';
 
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      injectRandRestList(dataHandler(arrayFromJson));
+
+      // gives us the randomly generated restaurant list
+      currentArray = dataHandler(arrayFromJson);
+      injectRandRestList(currentArray);
     });
 
-    let currentArray;
-    restNameInput.addEventListener('input', (e) => {
+    let currentArray = [];
+    restNameInput.addEventListener('input', async (e) => {
       console.log(e.target.value);
+      // console.log(currentArray);
+      if (arrayFromJson.length < 1) {
+        console.log('empy');
+        return;
+      }
+
+      const targetRest = currentArray.filter((item) => {
+        // console.log(item.name);
+        const lowerName = item.name.toLowerCase();
+        const lowerInput = e.target.value.toLowerCase();
+
+        return lowerName.includes(lowerInput);
+      });
+      console.log(targetRest);
+      injectRandRestList(targetRest);
+    });
+
+    zipCodeInput.addEventListener('input', (e) => {
+      console.log(e.target.value);
+
+      if (arrayFromJson.length < 1) {
+        console.log('empty');
+        return;
+      }
+
+      let targetZip = currentArray.filter((item) => {
+        // console.log(item.zip);
+        return item.zip.includes(e.target.value);
+      });
+
+      console.log(targetZip);
+      injectRandRestList(targetZip);
     });
   }
 }
